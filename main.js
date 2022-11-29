ce7fba584cdb7b5c497d41a27e84a110c7e612ce
chetansource <chetankumargopi149@gmail.com>
2022-11-29 09:52:54 +0530

    retrive data back from local storage

--- main.js
+++ main.js
@@ -1,192 +1,205 @@
+let todoArray
+let todoID
 
-// window.addEventListener("load", () => {  /* the load event is fired when the page is completely loaded with all depedent resoure */
-//     const form = document.querySelector('#new-form')
-//     const input = document.querySelector('#new-task')
-//     const list_elements = document.querySelector('#loaded-tasks')
-
-//     // console.log(form)
-//     const myArray = []
-//     form.addEventListener('submit', (e) => {
-//         e.preventDefault() //this will stop from refreshing the page by deafult
-//         let task = input.value
-//         // console.log(task)
-//         if (!task) {                        // let and const reference types refer it , use const instead ad check it , preventdefault action
-//             alert('please fill out the task')       //functions to create delete element for each element
-//             return
-//         }
-//         const task_ele = document.createElement('div')
-
-
-//         const task_input_ele = document.createElement('input')
-//         task_input_ele.type = 'text'
-//         task_input_ele.value = task
-//         task_ele.appendChild(task_input_ele)
-//         list_elements.appendChild(task_ele)
-
-//         const task_actions = document.createElement('div')
-//         const task_edit_ele = document.createElement('button')
-//         task_edit_ele.innerHTML = "Edit"
-//         const task_delete_ele = document.createElement('button')
-//         task_delete_ele.innerHTML = "Delete"
-
-
-//         task_actions.appendChild(task_edit_ele)
-//         task_actions.appendChild(task_delete_ele)
-//         task_ele.appendChild(task_actions)
-
-//         input.value = ""
-
-//         task_delete_ele.addEventListener('click', () => {
-//             list_elements.removeChild(task_ele)
-//         })
-
-
-//         let myObj = {
-//             myTask: task
-//         }
-//         myArray.push(myObj)
-
-//         // storing in localstorage
-//         let myObj_string = JSON.stringify(myArray)
-//         localStorage.setItem('myData', myObj_string)
-//         console.log(localStorage)
-
-
-
-
-//     })
-
-// });
-
-//selector
-const todoInput = document.querySelector('.todo-input')
-const todoButton = document.querySelector('.todo-button')
-const todoList=document.querySelector(".todo-list")
-// const edit=document.querySelectorAll(".todo-list li")
-
-//Event Listeners
-
-todoButton.addEventListener('click', (event)=>{
- event.preventDefault();
- if(todoInput.value === ""){
-    alert("please add a todo")
-    return
- }
- addTodo();
-});
-
-todoList.addEventListener('click', deleteCheckEdit)
-
-
-
-
-//functions
-
-
-function addTodo(){
-  
-    const todoDiv=document.createElement("div") //creating div
-    todoDiv.classList.add("todo")
-    const todoProperties=document.createElement("div")
-    to
-    const newTodo=document.createElement('li') //creating li to add todo
-    newTodo.innerText=todoInput.value
-    saveLocalTodos(todoInput.value)
-    newTodo.classList.add("todo-item")
-    todoDiv.appendChild(newTodo)
-
-    
-
-    //check mark button
-    const checkbutton= checkButton()
-    todoDiv.appendChild(checkbutton)
-
-    //edit button
-    const editbutton=document.createElement("button")
-    editbutton.innerHTML='<i class="fa-regular fa-pen-to-square"></i>'
-    editbutton.classList.add("editBtn")
-    todoDiv.appendChild(editbutton)
-
-    //delete button
-    const deletebutton=document.createElement('button')
-    deletebutton.innerHTML='<i class="fa-solid fa-trash-can"></i>'
-    deletebutton.classList.add('deleteBtn')
-    todoDiv.appendChild(deletebutton)
-
-    todoList.appendChild(todoDiv)
-
-    todoInput.value=""
-
-   newTodo.addEventListener('click',(event)={
-
-   })
+try {
+  todoArray = JSON.parse(window.localStorage.getItem('todoArray'))
+  todoID = todoArray.map(item => item.id).reduce((i, j) => { if (i > j) return i; else { return j } }, 0) + 1
+  showTodo()
+} catch (error) {
+  todoArray = []
+  todoID = 0
 }
 
-function checkButton () {
-    const checkbutton=document.createElement('button')
-    checkbutton.innerHTML='<i class="fas fa-check"></i>'
-    checkbutton.classList.add("completeBtn")
-    return checkbutton
+const updateLocalStorage = () => {
+  window.localStorage.setItem('todoArray', JSON.stringify(todoArray))
 }
 
-function deleteCheckEdit(e){
-    const item=e.target //target returns the element when the event is triggered
-    // console.log(item )
-    if(item.classList[0] === "deleteBtn"){
-        const todo=item.parentElement  //one difference between parentNode and parentElement if a element doesnt have a parent PE return as null PN will return something
-        todo.remove()
-    }
+const button = document.getElementById('submitTodo')
+button.addEventListener('click', (event) => {
+  event.preventDefault()
+  const input = document.getElementById('inputText')
+  if (input.value.length !== 0) {
+    todoArray.push({ id: todoID, text: input.value })
+    todoID++
 
-    if(item.classList[0] === "completeBtn"){
-        const todo=item.parentElement
-        todo.classList.toggle("done")
-    }
+    input.value = '' // setting the input value to empty after submit
 
-    if(item.classList[0] === "editBtn"){
-        const todo=prompt('you can edit the field',item.parentElement.firstChild.innerText)
-        item.parentElement.firstChild.innerText=todo
-    }
-    
+    showTodo()
+    updateLocalStorage()
+  }
+})
 
-}
-const todos=[]
-function saveLocalTodos(todo){
-    const todoObj={
-        myTodo:todo
-    }
-    todos.push(todoObj)
-    console.log(todos)
-    localStorage.setItem("todos",JSON.stringify(todos))
-
+function showTodo () {
+  const todoContainer = document.querySelector('.todoContainer')
+  todoContainer.textContent = ''
+  todoArray.forEach((todo) => {
+    todoContainer.appendChild(createDivElement(todo))
+  })
 }
 
+function createDivElement (todo) {
+  const elementDiv = document.createElement('div')
+  elementDiv.id = todo.id
+  elementDiv.className = 'firstDiv'
 
+  const textInput = addTextInput(todo)
+  const checkBox = addCheckBox(todo)
+  const properties = addProperties(todo)
 
+  elementDiv.appendChild(textInput)
+  elementDiv.appendChild(checkBox)
+  elementDiv.appendChild(properties)
 
+  elementDiv.addEventListener('click', (event) => {
+    const element = event.target.tagName
+    // console.log(element)
+    if (element === 'TEXTAREA' || element === 'SELECT') { return }
+    if (properties.style.display === 'none') {
+      properties.style.display = 'block'
+    } else {
+      properties.style.display = 'none'
+    }
+  })
 
+  return elementDiv
+}
 
+function addTextInput (todo) {
+  const newTodo = document.createElement('input')
+  newTodo.type = 'text'
+  newTodo.value = todo.text
+  newTodo.id = 'input' + String(todo.id)
+  newTodo.className = 'todo-title'
+  // newTodo.style.backgroundColor = 'blue'
+  newTodo.addEventListener('change', () => {
+    todo.text = newTodo.value
+    updateLocalStorage()
+  })
+  return newTodo
+}
 
+function addCheckBox (todo) {
+  const checkBox = document.createElement('input')
+  checkBox.type = 'checkbox'
+  // console.log(todo.checkBox)
+  console.log(checkBox.checked)
+  if (todo.checkBox) {
+    checkBox.checked = todo.checkBox
+  }
+  console.log(checkBox.checked)
+  checkBox.addEventListener('change', () => {
+    todo.checkBox = checkBox.checked
+    const element = document.getElementById('input' + String(todo.id))
+    if (todo.checkBox === true) {
+      element.style.textDecoration = 'line-through'
+    } else {
+      element.style.textDecoration = 'none'
+    }
+    updateLocalStorage()
+  })
+  return checkBox
+}
 
+function addProperties (todo) {
+  const textArea = addTextArea(todo)
+  const addDateLabel = createDateLabel(todo)
+  const addDate = createDate(todo)
+  const priorityLabel = addPriorityLabel(todo)
+  const priority = addPriority(todo)
+  const deleteBtn = createDeleteBtn(todo)
 
+  const hideDiv = document.createElement('div')
+  hideDiv.className = 'hideDiv'
+  hideDiv.style.display = 'none'
 
+  hideDiv.appendChild(textArea)
+  hideDiv.appendChild(addDateLabel)
+  hideDiv.appendChild(addDate)
+  hideDiv.appendChild(priorityLabel)
+  hideDiv.appendChild(priority)
+  hideDiv.appendChild(deleteBtn)
 
+  return hideDiv
+}
 
+function addTextArea (todo) {
+  const textArea = document.createElement('textarea')
+  textArea.placeholder = 'Notes'
+  textArea.className = 'text-area'
+  if (todo.textArea !== undefined) {
+    textArea.value = todo.textArea
+  }
+  textArea.addEventListener('change', () => {
+    todo.textArea = textArea.value
+    updateLocalStorage()
+  })
 
+  return textArea
+}
 
+function createDateLabel (todo) {
+  const dateLabel = document.createElement('label')
+  dateLabel.textContent = 'Due Date:'
+  dateLabel.className = 'dateLabel'
+  dateLabel.setAttribute('for', 'date' + String(todo.id))
+  return dateLabel
+}
 
+function createDate (todo) {
+  const dateInput = document.createElement('input')
+  dateInput.type = 'date'
+  dateInput.className = 'add-date'
+  dateInput.id = 'date' + String(todo.id)
+  if (todo.dateInput !== undefined) {
+    dateInput.value = todo.dateInput
+  } else {
+    dateInput.valueAsDate = new Date()
+    todo.dateInput = dateInput.value
+  }
+  dateInput.addEventListener('change', () => {
+    todo.dateInput = dateInput.value
+    updateLocalStorage()
+  })
+  return dateInput
+}
+function addPriorityLabel (todo) {
+  const priorityLabel = document.createElement('label')
+  priorityLabel.textContent = 'priority:'
+  priorityLabel.className = 'priorityLabel'
+  priorityLabel.setAttribute('for', 'priority' + String(todo.id))
+  return priorityLabel
+}
 
+function addPriority (todo) {
+  const items = ['None', 'High', 'Medium', 'Low']
+  const Priority = document.createElement('select')
+  Priority.className = 'prior'
+  Priority.id = 'prior' + String(todo.id)
+  for (let i = 0; i < items.length; i++) {
+    const element = items[i]
+    const options = document.createElement('option')
+    options.textContent = element
+    Priority.appendChild(options)
+  }
+  if (todo.Priority !== undefined) {
+    Priority.value = todo.Priority
+  }
+  Priority.addEventListener('change', () => {
+    todo.Priority = Priority.value
+    updateLocalStorage()
+  })
+  return Priority
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function createDeleteBtn (todo) {
+  const deleteBtn = document.createElement('button')
+  deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+  deleteBtn.className = 'Delete'
+  const delID = todo.id
+  deleteBtn.addEventListener('click', () => {
+    document.getElementById(delID).remove()
+    todoArray = todoArray.filter(i => i.id !== delID)
+    updateLocalStorage()
+  })
+  return deleteBtn
+}
